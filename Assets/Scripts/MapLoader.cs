fd076a9fdf090a6da11d6e60bca0a7554662e7d5
Abacysta <noconfilip7@gmail.com>
2024-07-30 16:32:03 +0200

    Added happiness and population map.

--- Assets/Scripts/MapLoader.cs
+++ Assets/Scripts/MapLoader.cs
@@ -11,6 +11,17 @@ public class MapLoader : MonoBehaviour
     [SerializeField] private TileBase occupied_tile;
     [SerializeField] private TileBase water_tile;
     [SerializeField] private TileBase capital_tile;
+    [SerializeField] private TileBase sad_tile;
+    [SerializeField] private TileBase neutral_tile;
+    [SerializeField] private TileBase smile_tile;
+    [SerializeField] private TileBase happy_tile;
+
+    [SerializeField] private TileBase population_0_tile;
+    [SerializeField] private TileBase population_1_tile;
+    [SerializeField] private TileBase population_2_tile;
+    [SerializeField] private TileBase population_3_tile;
+    [SerializeField] private TileBase population_4_tile;
+    [SerializeField] private TileBase population_5_tile;
 
     void Start()
     {
@@ -94,6 +105,110 @@ public class MapLoader : MonoBehaviour
             tile_map_layer_1.SetColor(position, color);
         }
     }
+        void set_happiness()
+    {
+        tile_map_layer_1.ClearAllTiles();
+        tile_map_layer_2.ClearAllTiles();
+
+        foreach (Province province in map.Provinces)
+        {
+            Vector3Int position = new(province.X, province.Y, 0);
+            if (province.Type == "land")
+            {
+                if (province.Happiness < 25)
+                {
+                    tile_map_layer_1.SetTile(position, base_tile);
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(220, 20, 60)); // red - crimson
+                    tile_map_layer_2.SetTile(position, sad_tile);
+                    tile_map_layer_2.SetColor(position, choose_rgb_color(255, 255, 255, 150)); // transparency
+                }
+                else if (province.Happiness < 50)
+                {
+                    tile_map_layer_1.SetTile(position, base_tile);
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(225, 225, 0)); // yellow
+                    tile_map_layer_2.SetTile(position, neutral_tile);
+                    tile_map_layer_2.SetColor(position, choose_rgb_color(255, 255, 255, 150)); // transparency
+                }
+                else if (province.Happiness < 75)
+                {
+                    tile_map_layer_1.SetTile(position, base_tile);
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(0, 255, 0)); // green - lime
+                    tile_map_layer_2.SetTile(position, smile_tile);
+                    tile_map_layer_2.SetColor(position, choose_rgb_color(255, 255, 255, 150)); // transparency
+                }
+                else
+                {
+                    tile_map_layer_1.SetTile(position, base_tile);
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(0, 100, 0)); // dark green
+                    tile_map_layer_2.SetTile(position, happy_tile);
+                    tile_map_layer_2.SetColor(position, choose_rgb_color(255, 255, 255, 150)); // transparency
+                }
+            }
+            else
+            {
+                tile_map_layer_1.SetTile(position, base_tile);
+                tile_map_layer_1.SetColor(position, choose_rgb_color(60, 106, 130)); // blue
+                tile_map_layer_2.SetTile(position, water_tile);
+            }
+        }
+    }
+
+    void set_population()
+    {
+        set_land_and_water();
+
+        foreach (Province province in map.Provinces)
+        {
+            Vector3Int position = new(province.X, province.Y, 0);
+            if (province.Type == "land")
+            {
+                /* Color populationColor = GetColorBasedOnPopulation(province.Population);
+                tile_map_layer_1.SetTile(position, base_tile);
+                tile_map_layer_1.SetColor(position, populationColor);
+                */
+                if(province.Population < 100){
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(255,155,133)); // coral pink
+                    tile_map_layer_2.SetTile(position, population_0_tile);
+                }
+                else if(province.Population < 200){
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(255,122,92)); // bittersweet 
+                    tile_map_layer_2.SetTile(position, population_1_tile);
+                }
+                else if(province.Population < 300){
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(255,88,51)); // tomato
+                    tile_map_layer_2.SetTile(position, population_2_tile);
+                }
+                else if(province.Population < 400){
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(255,55,10)); // coquelicot
+                    tile_map_layer_2.SetTile(position, population_3_tile);
+                }
+                else if(province.Population < 500){
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(224,41,0)); // sinopia
+                    tile_map_layer_2.SetTile(position, population_4_tile);
+                }
+                else {
+                    tile_map_layer_1.SetColor(position, choose_rgb_color(184,34,0)); // engineering orange 
+                    tile_map_layer_2.SetTile(position, population_5_tile);
+                }
+            }
+        }
+    }
+
+Color GetColorBasedOnPopulation(int population)
+{
+    // Define the range for the colors
+    int minPopulation = 0;
+    int maxPopulation = 600;
+
+    // Define colors for the range
+    Color minColor = choose_rgb_color(255,165,0); // orange 
+    Color maxColor = choose_rgb_color(165,42,42); //brown - chocolate
+
+
+    // Interpolate between yellow and brown
+    float t = Mathf.InverseLerp(minPopulation, maxPopulation, population);
+    return Color.Lerp(minColor, maxColor, t);
+}
 
     Color choose_rgb_color(int r, int g, int b, int a = 255)
     {
