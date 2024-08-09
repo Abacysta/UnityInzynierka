cbb1b82d3c50e1bd754ba18d1eefa738ef6b77dd
Musmike <michal.muzyka2002@gmail.com>
2024-08-09 16:37:04 +0200

    Fixed dialog box and resolved MonoBehaviour warning issue
    Adjusted dialog box for different resolutions and fixed warning related to instantiating MonoBehaviour-derived objects with 'new

--- Assets/ui/scripts/province_interface.cs
+++ Assets/ui/scripts/province_interface.cs
@@ -1,51 +1,55 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class province_interface : MonoBehaviour
 {
-    private class provinceInfoField:MonoBehaviour {
-        [SerializeField]
-        public TMP_Text txt;
-        [SerializeField]
-        public Image img;
+    private class ProvinceInfoField 
+    {
+        private TMP_Text txt;
+        private Image img;
 
-        public provinceInfoField(GameObject obj) {
-            txt = obj.GetComponentInChildren<TMP_Text>();
-            img = obj.GetComponentInChildren<Image>();
+        public ProvinceInfoField(GameObject obj) {
+            this.txt = obj.GetComponentInChildren<TMP_Text>();
+            this.img = obj.GetComponentInChildren<Image>();
         }
+
+        public TMP_Text Txt { get => txt; set => txt = value; }
+        public Image Img { get => img; set => img = value; }
     }
 
-
-    public GameObject id, type, res, happ, pop, rec_pop, building_interface;
-    private provinceInfoField id_, type_ , res_, happ_, pop_, rec_pop_;
-    public Map map;
-    private int prov;
-    public Image b_1, b_2, b_3, b_4;
-    public Sprite[] b_1_spr, b_2_spr, b_3_spr, b_4_spr;
-    public Transform b_1_m, b_2_m, b_3_m, b_4_m;
-
+    [SerializeField] private GameObject id, type, res, happ, pop, rec_pop, building_interface;
+    [SerializeField] private Map map;
+    [SerializeField] private Image b_1, b_2, b_3, b_4;
+    [SerializeField] private Sprite[] b_1_spr, b_2_spr, b_3_spr, b_4_spr;
+    [SerializeField] private Transform b_1_m, b_2_m, b_3_m, b_4_m;
     //[SerializeField] private buildings_interface buildings_Interface;
 
-    private void Start() {
-        id_ = new provinceInfoField(id);
-        type_ = new provinceInfoField(type);
-        res_ = new provinceInfoField(res);
-        happ_ = new provinceInfoField(happ);
-        pop_ = new provinceInfoField(pop);
-        rec_pop_ = new provinceInfoField (rec_pop);
-        
-        id_.txt.SetText("null");
-        type_.txt.SetText("null");
-        res_.txt.SetText("null");
-        happ_.txt.SetText("null");
-        pop_.txt.SetText("null");
-        rec_pop_.txt.SetText("null");
+    private ProvinceInfoField id_, type_, res_, happ_, pop_, rec_pop_;
+    private int prov;
 
-        foreach(var bt in new List<(Transform, BuildingType)> { (b_1_m, BuildingType.Infrastructure), (b_2_m, BuildingType.Fort), (b_3_m, BuildingType.School), (b_4_m, BuildingType.Mine) }) {
+    private void Start() {
+        id_ = new ProvinceInfoField(id);
+        type_ = new ProvinceInfoField(type);
+        res_ = new ProvinceInfoField(res);
+        happ_ = new ProvinceInfoField(happ);
+        pop_ = new ProvinceInfoField(pop);
+        rec_pop_ = new ProvinceInfoField (rec_pop);
+        
+        id_.Txt.SetText("null");
+        type_.Txt.SetText("null");
+        res_.Txt.SetText("null");
+        happ_.Txt.SetText("null");
+        pop_.Txt.SetText("null");
+        rec_pop_.Txt.SetText("null");
+
+        foreach(var bt in new List<(Transform, BuildingType)> { 
+                                    (b_1_m, BuildingType.Infrastructure), 
+                                    (b_2_m, BuildingType.Fort), 
+                                    (b_3_m, BuildingType.School), 
+                                    (b_4_m, BuildingType.Mine) 
+                                }) {
             bt.Item1.Find("add").GetComponent<Button>().onClick.AddListener(() => map.upgradeBuilding(map.Selected_province, bt.Item2));
             bt.Item1.Find("remove").GetComponent<Button>().onClick.AddListener(() => map.downgradeBuilding(map.Selected_province, bt.Item2));
         }
@@ -55,8 +59,8 @@ public class province_interface : MonoBehaviour
     private void Update() {
         var coordinates = map.Selected_province;
         Province p = map.getProvince(coordinates);
-        id_.txt.SetText (coordinates.ToString()+ (p.Owner_id!=0 ? " " + map.Countries[p.Owner_id].Name : ""));
-        type_.txt.SetText (""+p.Type);
+        id_.Txt.SetText (coordinates.ToString()+ (p.Owner_id!=0 ? " " + map.Countries[p.Owner_id].Name : ""));
+        type_.Txt.SetText (""+p.Type);
 
         if(p.Type == "land") {
             res.SetActive(true);
@@ -65,12 +69,17 @@ public class province_interface : MonoBehaviour
             rec_pop.SetActive(true);
             building_interface.SetActive(true);
 
-            res_.txt.SetText("" + p.Resources);
-            happ_.txt.SetText("" + p.Happiness);
-            pop_.txt.SetText("" + p.Population);
-            rec_pop_.txt.SetText("" + p.RecruitablePopulation);
+            res_.Txt.SetText("" + p.Resources);
+            happ_.Txt.SetText("" + p.Happiness);
+            pop_.Txt.SetText("" + p.Population);
+            rec_pop_.Txt.SetText("" + p.RecruitablePopulation);
 
-            var bld = new List<Building> { p.Buildings.Find(b => b.BuildingType == BuildingType.Infrastructure), p.Buildings.Find(b => b.BuildingType == BuildingType.Fort), p.Buildings.Find(b => b.BuildingType == BuildingType.School), p.Buildings.Find(b => b.BuildingType == BuildingType.Mine) };
+            var bld = new List<Building> { 
+                p.Buildings.Find(b => b.BuildingType == BuildingType.Infrastructure), 
+                p.Buildings.Find(b => b.BuildingType == BuildingType.Fort), 
+                p.Buildings.Find(b => b.BuildingType == BuildingType.School), 
+                p.Buildings.Find(b => b.BuildingType == BuildingType.Mine) 
+            };
             
             b_1.sprite = b_1_spr[bld[0].BuildingLevel];
             b_2.sprite = b_2_spr[bld[1].BuildingLevel];
