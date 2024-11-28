bb35f90a2aad1fc0acae90a6467cd903b7f70104
Musmike <michal.muzyka2002@gmail.com>
2024-11-28 23:34:20 +0100

    Fix build error

--- Assets/ui/scripts/country_interface/Mosframe/Editor/DynamicScrollViewEditor.cs
+++ Assets/ui/scripts/country_interface/Mosframe/Editor/DynamicScrollViewEditor.cs
@@ -6,31 +6,31 @@
  */
 namespace Mosframe {
 
-    using System;
-    using System.Collections.Generic;
-    using UnityEditor;
-    using UnityEngine;
+    //using System;
+    //using System.Collections.Generic;
+    //using UnityEngine;
+    //using UnityEditor;
 
-    /// <summary>
-    /// <see cref="DynamicScrollView"/> Editor
-    /// </summary>
-    public class DynamicScrollViewEditor {
+    ///// <summary>
+    ///// <see cref="DynamicScrollView"/> Editor
+    ///// </summary>
+    //public class DynamicScrollViewEditor {
 
 
-        [MenuItem( "GameObject/UI/Dynamic H Scroll View" )]
-        public static void CreateHorizontal () {
+    //    [MenuItem( "GameObject/UI/Dynamic H Scroll View" )]
+    //    public static void CreateHorizontal () {
 
-            var go = new GameObject( "Horizontal Scroll View", typeof(RectTransform) );
-            go.transform.SetParent( Selection.activeTransform, false );
-            go.AddComponent<DynamicHScrollView>().init();
-        }
+    //        var go = new GameObject( "Horizontal Scroll View", typeof(RectTransform) );
+    //        go.transform.SetParent( Selection.activeTransform, false );
+    //        go.AddComponent<DynamicHScrollView>().init();
+    //    }
 
-        [MenuItem( "GameObject/UI/Dynamic V Scroll View" )]
-        public static void CreateVertical () {
+    //    [MenuItem( "GameObject/UI/Dynamic V Scroll View" )]
+    //    public static void CreateVertical () {
 
-            var go = new GameObject( "Vertical Scroll View", typeof(RectTransform) );
-            go.transform.SetParent( Selection.activeTransform, false );
-            go.AddComponent<DynamicVScrollView>().init();
-        }
-    }
+    //        var go = new GameObject( "Vertical Scroll View", typeof(RectTransform) );
+    //        go.transform.SetParent( Selection.activeTransform, false );
+    //        go.AddComponent<DynamicVScrollView>().init();
+    //    }
+    //}
 }
